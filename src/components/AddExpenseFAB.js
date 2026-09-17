93294e6620272b0a6844fb1cc0fda2e734c2e76d
Senith Umesha <90299964+SenithUmesha@users.noreply.github.com>
2026-09-17 16:29:41 +0530

    refactor: make add expense button controlled

--- src/components/AddExpenseFAB.js
+++ src/components/AddExpenseFAB.js
@@ -1,71 +1,19 @@
-import { useState } from "react";
-
-import { Fab, Zoom, makeStyles } from "@material-ui/core";
+import { Fab, Zoom } from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
 
-import "../assets/AddExpenseFAB.css";
-
-const AddExpenseFAB = (props) => {
-  const useStyles = makeStyles({
-    buttonClicked: {
-      margin: 0,
-      top: "auto",
-      right: 20,
-      bottom: 20,
-      left: "auto",
-      position: "fixed",
-      zIndex: 20,
-      backgroundColor: "#e85a4f",
-      color: "#eae7dc",
-      "&:hover": {
-        backgroundColor: "#eae7dc",
-        color: "#e85a4f",
-      },
-    },
-    buttonNotClicked: {
-      margin: 0,
-      top: "auto",
-      right: 20,
-      bottom: 20,
-      left: "auto",
-      position: "fixed",
-      color: "#e85a4f",
-      zIndex: 20,
-      backgroundColor: "#eae7dc",
-      "&:hover": {
-        backgroundColor: "#e85a4f",
-        color: "#eae7dc",
-      },
-    },
-  });
-
-  const classes = useStyles();
-
-  const [isOpen, setIsOpen] = useState(true);
-
-  const clickHander = (event) => {
-    event.preventDefault();
-    setIsOpen(!isOpen);
-
-    if (isOpen) {
-      props.onClickFAB(true);
-    } else {
-      props.onClickFAB(false);
-    }
-  };
-
+const AddExpenseFAB = ({ isOpen, onClickFAB }) => {
   return (
-    <div className="expense-fab">
-      <Zoom in={true} timeout={{ enter: 500, exit: 500 }} unmountOnExit>
-        <Fab
-          className={!isOpen ? classes.buttonClicked : classes.buttonNotClicked}
-          size="large"
-          onClick={clickHander}
-        >
-          <AddIcon fontSize="large" />
-        </Fab>
-      </Zoom>
-    </div>
+    <Zoom in timeout={{ enter: 350, exit: 200 }}>
+      <Fab
+        className={`expense-fab ${isOpen ? "expense-fab-open" : ""}`}
+        size="large"
+        onClick={onClickFAB}
+        aria-label={isOpen ? "Close expense form" : "Add expense"}
+        aria-expanded={isOpen}
+      >
+        <AddIcon className="expense-fab-icon" fontSize="large" />
+      </Fab>
+    </Zoom>
   );
 };
 
